a6fc7e768cbf4d43117365ec2f1bd300247d8dfd
terrablue <102580937+terrablue@users.noreply.github.com>
2023-02-07 23:34:08 +0200

    [Fix] `Iterator`: throw when Iterator() is called without new

--- Iterator/implementation.js
+++ Iterator/implementation.js
@@ -1,8 +1,16 @@
 'use strict';
 
+var GetIntrinsic = require('get-intrinsic');
+
+var $TypeError = GetIntrinsic('%TypeError%');
+
 var iterProto = require('iterator.prototype');
 
-var $Iterator = typeof Iterator === 'function' ? Iterator : function Iterator() {};
+var $Iterator = typeof Iterator === 'function' ? Iterator : function Iterator() {
+	if (!(this instanceof Iterator)) {
+		throw new $TypeError('`Iterator` can only be called with new');
+	}
+};
 
 if ($Iterator.prototype !== iterProto) {
 	$Iterator.prototype = iterProto;
---
+++ test/Iterator.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var defineProperties = require('define-properties');
+var test = require('tape');
+var callBind = require('call-bind');
+var functionsHaveNames = require('functions-have-names')();
+
+var index = require('../Iterator');
+var impl = require('../Iterator/implementation');
+
+var isEnumerable = Object.prototype.propertyIsEnumerable;
+
+module.exports = {
+	tests: function (Iter, name, t) {
+		t.equal(typeof Iter, 'function', name + ' is a function');
+
+		t['throws'](
+			function () { Iter(); }, // eslint-disable-line new-cap
+			TypeError,
+			name + ' throws when Call-ed'
+		);
+	},
+	index: function () {
+		test('Iterator: index', function (t) {
+			module.exports.tests(index, 'Iterator', t);
+
+			t.end();
+		});
+	},
+	implementation: function () {
+		test('Iterator: implementation', function (t) {
+			module.exports.tests(callBind(impl, null), 'Iterator', t);
+
+			t.end();
+		});
+	},
+	shimmed: function () {
+		test('Iterator: shimmed', function (t) {
+			t.test('Function name', { skip: !functionsHaveNames }, function (st) {
+				st.equal(Iterator.name, 'Iterator', 'Iterator has name "Iterator"');
+				st.end();
+			});
+
+			t.test('enumerability', { skip: !defineProperties.supportsDescriptors }, function (et) {
+				et.equal(false, isEnumerable.call(global, Iterator), 'Iterator is not enumerable');
+				et.end();
+			});
+
+			module.exports.tests(Iterator, 'Iterator', t);
+
+			t.end();
+		});
+	}
+};
