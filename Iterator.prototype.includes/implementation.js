5b2ed34a032711250fe4f0923df81939a5db978e
Jordan Harband <ljharb@gmail.com>
2026-03-12 14:42:44 -0400

    [Fix] `includes`: close iterator when throwing
    See https://github.com/tc39/proposal-iterator-includes/pull/8

--- Iterator.prototype.includes/implementation.js
+++ Iterator.prototype.includes/implementation.js
@@ -8,6 +8,7 @@ var IteratorClose = require('es-abstract/2025/IteratorClose');
 var IteratorStepValue = require('es-abstract/2025/IteratorStepValue');
 var NormalCompletion = require('es-abstract/2025/NormalCompletion');
 var SameValueZero = require('es-abstract/2025/SameValueZero');
+var ThrowCompletion = require('es-abstract/2025/ThrowCompletion');
 
 var isInteger = require('math-intrinsics/isInteger');
 
@@ -25,44 +26,52 @@ module.exports = function includes(searchElement) {
 		throw new $TypeError('`this` value must be an Object'); // step 2
 	}
 
-	var skippedElements = arguments.length > 1 ? arguments[1] : undefined; // step 3
+	var iterated = { // step 3
+		'[[Iterator]]': O,
+		'[[NextMethod]]': undefined,
+		'[[Done]]': false
+	};
+
+	var skippedElements = arguments.length > 1 ? arguments[1] : undefined; // step 4
 
 	var toSkip = 0;
-	if (typeof skippedElements !== 'undefined') { // step 4
+	if (typeof skippedElements !== 'undefined') { // step 5
 		if (
 			typeof skippedElements !== 'number'
 			|| !isFinite(skippedElements)
 			|| !isInteger(skippedElements)
-		) {
-			throw new $TypeError('`skippedElements` must be a finite integral Number'); // step 4.a
+		) { // step 4.a
+			var error = ThrowCompletion(new $TypeError('`skippedElements` must be a finite integral Number')); // step 4.a.1
+			return IteratorClose(iterated, error); // step 4.a.2
 		}
 
-		toSkip = skippedElements; // step 4.b
+		toSkip = skippedElements; // step 5.b
 	}
 
-	if (toSkip < 0) {
-		throw new $RangeError('`skippedElements` must be >= 0'); // step 5
+	if (toSkip < 0) { // step 6
+		var error2 = ThrowCompletion(new $RangeError('`skippedElements` must be >= 0')); // step 6.a
+		return IteratorClose(iterated, error2); // step 6.b
 	}
 
-	var skipped = 0; // step 6
+	var skipped = 0; // step 7
 
-	var iterated = GetIteratorDirect(O); // step 7
+	iterated = GetIteratorDirect(O); // step 8
 
-	while (true) { // step 8
-		var value = IteratorStepValue(iterated); // step 8.a
+	while (true) { // step 9
+		var value = IteratorStepValue(iterated); // step 9.a
 
 		if (iterated['[[Done]]']) {
-			return false; // step 8.b
+			return false; // step 9.b
 		}
-		if (skipped < toSkip) { // step 3
-			skipped += 1; // step 3.a
+		if (skipped < toSkip) { // step 4
+			skipped += 1; // step 4.a
 		} else {
 			// eslint-disable-next-line no-lonely-if
-			if (SameValueZero(value, searchElement)) { // step 8.d
+			if (SameValueZero(value, searchElement)) { // step 9.d
 				return IteratorClose(
 					iterated,
 					NormalCompletion(true)
-				); // step 8.d.i
+				); // step 9.d.i
 			}
 		}
 	}
